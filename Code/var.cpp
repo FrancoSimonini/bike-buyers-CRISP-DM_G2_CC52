ef9b7e145b3312535e28ef338ca121a77eeae8ba
ibrahimuwu <u20191E650@upc.edu.pe>
2022-11-19 21:21:05 -0500

    upload code cleaning database for 7,8,3 problem

---
+++ Code/var.cpp
@@ -0,0 +1,17 @@
+La base de datos tiene registros de 1000 usuarios de diferentes orígenes y si compran o no una bicicleta.
+
+
+ID: el ID de los compradores.
+Estado Civil: El estado del comprador de ser casado o soltero.
+Género: El estado del comprador, ya sea que el comprador sea hombre o mujer.
+Ingresos: Ingresos del comprador en un tiempo determinado.
+Hijos: El número de hijos que tiene el comprador.
+Educación: Antecedentes educativos del comprador, ya sea licenciatura, título de posgrado, escuela secundaria, universidad parcial o escuela secundaria parcial.
+Ocupación: El trabajo u ocupación del comprador, ya sea oficinista, administrativo, manual, profesional o manual especializado.
+Propietario de vivienda: Estado del comprador si el comprador tiene o no su propia casa.
+Autos: La cantidad de autos que tiene el comprador.
+Distancia de viaje: La distancia entre la casa del comprador y la empresa del comprador. Hay 5 categorías; 0-1 Millas, 1-2 Millas 10+ Millas, 2-5 Millas y 5-10 Millas.
+Región: Variable que nos dice dónde vive el comprador, ya sea en Europa, América del Norte o el Pacífico.
+Edad: La edad del comprador.
+Bicicleta comprada: Estado del comprador si el comprador compró la bicicleta o no.
+Variable dependiente Y: Bicicleta Comprada
